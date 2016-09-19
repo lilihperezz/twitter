ecf0cabd85ab5f554f4b285075541fdb322d9e1b
lili hualtibamba <lilihperezz@gmail.com>
2016-09-19 16:03:37 -0500

    agregando cambios

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
 
 window.addEventListener("load", function() {
    var button = document.getElementById("calcular");
-   button.addEventListener("click", function() {
-       	  			      
+   button.addEventListener("click", function() {     	  			      
 	function myFunction() {
-	var textArea= document.getElementById("textArea").value;
-    var newItem = document.createElement("p");
-    var textnode = document.createTextNode("Tweet "+ textArea );
-    newItem.appendChild(textnode);
-    var resultado = document.getElementById("resultado");
-    resultado.insertBefore(newItem,resultado.childNodes[0]);
-   }
+	     var textArea= document.getElementById("textArea").value;
+         var newItem = document.createElement("p");
+         var textnode = document.createTextNode("Tweet "+ textArea );
+         newItem.appendChild(textnode);
+         var resultado = document.getElementById("resultado");
+         resultado.insertBefore(newItem,resultado.childNodes[0]);
+        }
      myFunction();
 	});    
 });
